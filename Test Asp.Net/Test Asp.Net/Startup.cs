864bf799516ffda86ccc05dcf233784cf7c35b98
Peter Motycak <motycak@gmail.com>
2015-12-10 22:59:22 +0100

    Vytvorenie projektu

---
+++ Test Asp.Net/Test Asp.Net/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Test_Asp.Net.Startup))]
+namespace Test_Asp.Net
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
